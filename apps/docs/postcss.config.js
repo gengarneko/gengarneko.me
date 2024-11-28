f68e0efe2320bba017ad5e9723c58a61daf451b2
gengarneko <2234763217@qq.com>
2024-11-28 12:06:42 +0800

    feat(docs): finish footer comp with tailwindcss

--- apps/docs/postcss.config.js
+++ apps/docs/postcss.config.js
@@ -1,9 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
   },
 };
-
-export default config;
--- apps/docs/rspress.config.ts
+++ apps/docs/rspress.config.ts
@@ -2,9 +2,6 @@ import * as path from 'node:path';
 
 import { defineConfig } from 'rspress/config';
 
-const AVATAR_URL =
-  'https://gravatar.com/avatar/237cbf6b0482ea079b8c98e85e2685a2?size=256&cache=1730712269518';
-
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'GengarNeko',
@@ -19,9 +16,10 @@ export default defineConfig({
       },
     ],
   },
+  globalStyles: path.join(__dirname, 'styles/index.css'),
   globalUIComponents: [
     [
-      path.join(__dirname, 'components', 'MyComponent.tsx'),
+      path.join(__dirname, 'components', 'footer.tsx'),
       {
         foo: 'bar',
       },
